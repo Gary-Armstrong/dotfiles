4b7d08a095db3f963aa11950225d113417a082fe
Gary Armstrong <gary.armstrong@weather.com>
2015-11-10 09:21:04 -0500

    Initial commit

---
+++ set.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# set links
+# make better later
+
+/bin/ln -sf ~/dotfiles/ssh/config ~/.ssh/config
+/bin/ln -sf ~/dotfiles/bash_profile ~/.bash_profile
+/bin/ln -sf ~/dotfiles/vim ~/.vim
+/bin/ln -sf ~/dotfiles/vimrc ~/.vimrc
